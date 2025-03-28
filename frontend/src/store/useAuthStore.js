0c42c1b7cef2e13a24bd6f125e3b7ae7da13eb0d
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-03-29 00:35:10 +0530

    updated authstore baseurl

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -22,7 +22,7 @@ export const useAuthStore = create((set, get) => ({
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
-      console.log("Error in checkAuth:", error);
+      console.log("Error in checkAuth:", error.response.data);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
