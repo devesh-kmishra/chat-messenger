c9b378c108e86b586c5b9b5ecd32ded5218484a1
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-03-29 17:13:37 +0530

    updated axios baseurl

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:3000/api"
+//       : "https://chat-messenger-ten.vercel.app/api",
+//   withCredentials: true,
+// });
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "https://chat-messenger-ten.vercel.app/api",
+  baseURL: "https://chat-messenger-ten.vercel.app/api",
   withCredentials: true,
 });
