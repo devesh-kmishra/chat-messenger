44c907b0995b467e214a9569c07ec3a72b7f000b
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-03-29 21:58:50 +0530

    removed dist folder

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,35 +2,25 @@ import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 import { connectDB } from "../src/lib/db.js";
 import authRoutes from "../src/routes/auth.route.js";
 import messageRoutes from "../src/routes/message.route.js";
 import { app, server } from "../src/lib/socket.js";
 
 dotenv.config();
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(process.env.PORT, () => {
   console.log("Server is running on port:" + process.env.PORT);
