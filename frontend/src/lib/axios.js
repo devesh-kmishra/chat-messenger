b069d35008be5c7a0be0c57e2d4a0a784c8487f1
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-03-30 21:33:36 +0530

    updated env backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "https://chat-messenger-backend.vercel.app/api",
+  baseURL: `${import.meta.env.BACKEND_URL}/api`,
   withCredentials: true,
 });
